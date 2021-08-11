096ad2e34af2f1a881d0af652858988f7f4b5b5f
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-08-10 21:32:06 -0400

    implemented react router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,26 @@
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 const Header = () => {
   return (
     <header>
       <Navbar bg='light' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>AwesomeCaps</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>AwesomeCaps</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
